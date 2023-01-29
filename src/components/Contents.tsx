b21756b6e773208e980f0fbc3558f88ef660c8e4
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-29 10:12:12 +0900

    Chore : 필요없는 fragment 삭제

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -2,11 +2,9 @@ const Contents = () => {
   return (
     <section className="border-2 flex flex-col items-center gap-4 pb-10 bg-[#e2e2e2a1] ">
       {[...Array(10)].map((_, i) => (
-        <>
-          <div key={i} className="w-96 h-10 mt-3 rounded-lg bg-white">
-            <div className="bg-gray-600 w-10 h-10 relative top-0 rounded-md"></div>
-          </div>
-        </>
+        <div key={i} className="w-96 h-10 mt-3 rounded-lg bg-white">
+          <div className="bg-gray-600 w-10 h-10 relative top-0 rounded-md"></div>
+        </div>
       ))}
     </section>
   );
