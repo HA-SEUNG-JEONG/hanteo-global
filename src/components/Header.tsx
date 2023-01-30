229932a52640906acc7621a9d93112fc97ea4e8d
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-30 11:49:07 +0900

    Style : hover 이벤트 적용 - opacity 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="bg-black w-[420px]">
-      <p className="text-white">9:41</p>
-      <section className="bg-[#FFB1B2] flex space-x-10 font-bold justify-center">
-        <article>차트</article>
-        <article>Whook</article>
-        <article>이벤트</article>
-        <article>뉴스</article>
-        <article>스토어</article>
+      <p className="text-white ml-3 py-1 font-bold">9:41</p>
+      <section className="bg-[#FFB1B2] flex space-x-10 font-bold justify-center py-1">
+        <Link to="/charts">
+          <article className="hover:opacity-30">차트</article>
+        </Link>
+        <Link to="/whook">
+          <article className="hover:opacity-30">Whook</article>
+        </Link>
+        <Link to="/event">
+          <article className="hover:opacity-30">이벤트</article>
+        </Link>
+        <Link to="/news">
+          <article className="hover:opacity-30">뉴스</article>
+        </Link>
+        <Link to="/store">
+          <article className="hover:opacity-30">스토어</article>
+        </Link>
       </section>
     </header>
   );
