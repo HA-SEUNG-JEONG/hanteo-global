03eea9087d192c973965c158a0277a574b6f8657
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-30 11:35:56 +0900

    Style : footer 스타일 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,7 @@
-const Footer = () => <footer className="bg-[#E1E1E1] w-[420px] h-10">1</footer>;
+const Footer = () => (
+  <footer className="font-bold bg-[#E1E1E1] w-[420px] h-10 flex justify-center items-center">
+    Hanteo Global
+  </footer>
+);
 
 export default Footer;
